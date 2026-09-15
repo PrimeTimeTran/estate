71589ea844d6c4ff057ac192c6a06c0f193d0752
PrimeTimeTran <primetimetran@gmail.com>
2026-09-15 14:56:36 -0400

    Cleanup

--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -2,8 +2,6 @@
 /// This should encapsulate logic that isn't "app" specific but 
 /// also encapsulates larger systems that are bigger than a single
 /// target. Event is a leading contender right now.
-/// 
-
 // Whats a mod? Something bigger than a mod, model, behavior.
 // Consider this dir for "event". It's both client, server, native.
 //
