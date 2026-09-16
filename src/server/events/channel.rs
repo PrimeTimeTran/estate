de56ead9bc8f2a7a858a1abe01c67747c97f29ea
PrimeTimeTran <primetimetran@gmail.com>
2026-09-15 21:01:05 -0400

    Cleanup

--- src/server/events/channel.rs
+++ src/server/events/channel.rs
@@ -1,4 +1,8 @@
 use tokio::sync::mpsc::error::TryRecvError;
+use tokio::sync::mpsc::{
+	Receiver as TokioReceiver, Sender as TokioSender, channel as tokio_channel,
+	error::{SendError, TrySendError},
+};
 
 use crate::prelude::*;
 
@@ -7,7 +11,7 @@ pub fn channel<T>(capacity: usize) -> (EventSender<T>, EventReceiver<T>) {
 	// EventSender<T> ─┼──> EventReceiver<T>
 	// EventSender<T> ─┘
 	// Multi Producer Single Consumer
-	let (tx, rx) = tokio::sync::mpsc::channel(capacity);
+	let (tx, rx) = tokio_channel(capacity);
 	(EventSender { tx }, EventReceiver { rx })
 }
 
@@ -36,25 +40,25 @@ impl<T> EventReceiver<T> {
 }
 
 impl<T> EventSender<T> {
-	pub async fn send(&self, event: T) -> Result<(), mpsc::error::SendError<T>> {
+	pub async fn send(&self, event: T) -> Result<(), SendError<T>> {
 		self.tx.send(event).await
 	}
-	pub fn try_send(&self, event: T) -> Result<(), mpsc::error::TrySendError<T>> {
+	pub fn try_send(&self, event: T) -> Result<(), TrySendError<T>> {
 		self.tx.try_send(event)
 	}
-	pub fn blocking_send(&self, event: T) -> Result<(), mpsc::error::SendError<T>> {
+	pub fn blocking_send(&self, event: T) -> Result<(), SendError<T>> {
 		self.tx.blocking_send(event)
 	}
 }
 
 #[derive(Debug)]
 pub struct EventSender<T> {
-	tx: tokio::sync::mpsc::Sender<T>,
+	tx: TokioSender<T>,
 }
 
 #[derive(Debug)]
 pub struct EventReceiver<T> {
-	rx: tokio::sync::mpsc::Receiver<T>,
+	rx: TokioReceiver<T>,
 }
 
 // pub struct NativeEventReceiver {
